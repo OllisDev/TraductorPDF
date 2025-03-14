a5f89c3d71d4ce0ff26d5f495eb2c8ca728a7adb
OllisDev <ollisfortnike@gmail.com>
2025-03-14 11:31:48 +0100

    funcionalidad servicio web con streamlit

--- app/app.py
+++ app/app.py
@@ -1,16 +1,39 @@
 import streamlit as st
 import pandas as pd
+import os
+import zipfile
+from pdf2image import convert_from_path
+from io import  BytesIO
+
+def pdf_to_images(pdf_file):
+    images = convert_from_path(pdf_file)
+    return images
+
+def create_zip(images):
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+        for i, img in enumerate(images):
+            img_bytes = BytesIO()
+            img.save(img_bytes, format = "JPEG")
+            zip_file.writestr(f"page_{i+1}.jpg", img_bytes.getvalue())
+    zip_buffer.seek(0)
+    return zip_buffer
  
 st.title("Traductor de documentos en Chino")
 uploaded_file = st.file_uploader("Sube un archivo PDF", type=["pdf"])
-if uploaded_file is not None:
-    st.success("¡Archivo subido correctamente!")
-    st.write(f"Nombre del archivo: {uploaded_file.name}")
-    st.write(f"Tamaño: {(uploaded_file.size /1028)} Kb")
+if uploaded_file:
+    with open("temp.pdf", "wb") as f:
+        f.write(uploaded_file.getbuffer())
  
-    if st.button("Procesar PDF"):
-        st.write("Aquí podrías mostrar el texto extraído o generar un nuevo archivo.")
- 
-    # Agregar botones y opciones adicionales
-    if st.button("Descargar PDF modificado"):
-        st.write("Aquí podrías generar un link para la descarga.")
+    st.success ("PDF cargado correctamente")
+
+    if st.button("Convertir a imágenes"):
+        images = pdf_to_images("temp.pdf")
+        zip_buffer = create_zip(images)
+
+        st.download_button(
+            label = "Descargar ZIP",
+            data = zip_buffer,
+            file_name = "imagenes_pdf.zip",
+            mime = "application/zip"
+        )
