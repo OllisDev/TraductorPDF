7895e15d3cadf82ef26008960443491a9798f9a5
OllisDev <ollisfortnike@gmail.com>
2025-03-14 10:40:31 +0100

    servicio web

--- app/app/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from app.ui import PDFToJPGTranslator
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = PDFToJPGTranslator()
-    window.show()
-    sys.exit(app.exec())
